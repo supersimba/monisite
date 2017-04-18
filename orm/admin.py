5583c8772fd2b4506cdc1799edaa415aec4d6cf7
supersimba <supersimba@yeah.net>
2017-04-18 16:30:01 +0800

    0418 16:29

--- orm/admin.py
+++ orm/admin.py
@@ -6,6 +6,13 @@ from orm.models import *
 
 class RepQueueAdmin(admin.ModelAdmin):
     list_display = ['describe','src_ip','src_path','tgt_ip','tgt_path', 'add_user','add_time']
+    # list_editable = ['describe','src_ip','src_path','tgt_ip','tgt_path', 'add_time']
+    def save_model(self, request, obj, form, change):
+        print request.user
+        if getattr(obj,'add_user',None):
+            obj.add_user=request.user
+            print 'obj.add_user:',obj.add_user
+            obj.save()
 
 
 admin.site.register(rep_queue,RepQueueAdmin)
--- orm/models.py
+++ orm/models.py
@@ -4,6 +4,11 @@ from django.db import models
 import datetime
 from django.contrib.auth.models import User,AbstractUser
 
+
+class ReqQueueManager(models.Manager):
+    def get_queryset(self):
+        return super(ReqQueueManager,self).get_queryset().filter(rid=6)
+
 class rep_queue(models.Model):
     rid=models.AutoField(primary_key=True)
     describe=models.CharField(max_length=100,blank=False,verbose_name=u'队列描述')
@@ -11,9 +16,12 @@ class rep_queue(models.Model):
     src_path=models.CharField(max_length=100,blank=False,verbose_name=u'源端路径')
     tgt_ip=models.GenericIPAddressField(blank=False,verbose_name=u'目标端IP')
     tgt_path = models.CharField(max_length=100, blank=False, verbose_name=u'目标端路径')
-    # add_user=models.CharField(max_length=50,blank=False,verbose_name=u'添加用户')
-    add_user=models.ForeignKey(User)
+    add_user=models.CharField(max_length=50,blank=False,verbose_name=u'添加用户',default=u'---------')
+    # add_user=models.ForeignKey(User)
     add_time=models.DateTimeField(verbose_name=u'信息添加时间',default=datetime.datetime.now)
+
+    objects=models.Manager()
+    req_queue_objects=ReqQueueManager()
 
     class Meta:
         db_table='rep_queue'
